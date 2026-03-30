93bbaca49c7b71c619f26c1cb1ed8e2b9767a800
QuangKhai <10.12t3nguyentranquangkhaicvt@gmail.com>
2026-03-30 22:42:43 +0700

    add bai7 folder

--- Lab05-Qlsp/src/main/java/bai4_qlsp/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package bai4_qlsp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import bai4_qlsp.model.Category;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
--- Lab05-Qlsp/src/main/java/bai4_qlsp/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package bai4_qlsp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import bai4_qlsp.model.Product;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
